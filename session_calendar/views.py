b753afa3042d320f0407d85d068c3b2f25587169
Muazzam Alseri <muazzam.alseri@maplelabs.com>
2023-03-10 11:21:32 +0530

    Added psycopg2 lib and code

--- session_calendar/views.py
+++ session_calendar/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+import psycopg2
+
+con = psycopg2.connect(
+    host = "localhost",
+    database = "hrm_db",
+    user = "postgres",
+    password = "",
+)
+
+cur = con.cursor()
+
+cur.execute("insert into calendar(training_name, trainer_name, employee_list, session_data) values('django', 'Raj', ARRAY [5, 7], '2023-03-10')")
+
+cur.fetchall()
+
+for r in rows:
+    print(r)
+
+cur.close()
+
+con.close()
