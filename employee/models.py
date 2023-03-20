d3b2f7e99b96d961c3aa490025b4162acb8e7fd3
Muazzam Alseri <muazzam.alseri@maplelabs.com>
2023-03-20 12:36:49 +0530

    modified employee/models.py

--- employee/models.py
+++ employee/models.py
@@ -4,7 +4,7 @@ class Employee(models.Model):
     emp_id = models.IntegerField(primary_key=True,unique=True)
     emp_name = models.TextField()
     emp_category = models.TextField()
-    rm_id = models.ForeignKey("self", on_delete=models.CASCADE,  null=True, blank=True)
+    rm = models.ForeignKey("self", on_delete=models.CASCADE,  null=True, blank=True)
 
     class Meta:
         db_table = 'employee'
