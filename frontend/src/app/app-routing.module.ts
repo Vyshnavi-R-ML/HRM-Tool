9031bd70b82b0906db779c2bd7c3f6a7c05dfe84
Muazzam Alseri <muazzam.alseri@maplelabs.com>
2023-03-29 11:15:14 +0530

    added routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'auth', component: AuthenticationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
