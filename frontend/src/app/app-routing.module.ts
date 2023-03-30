e17e06785600814268726d2a346cabab04c46767
Shiva-Pranav-ML <shiva.pranav@maplelabs.com>
2023-03-30 17:04:36 +0530

    modified styling

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './authentication/authentication.component';
 import { HomeComponent } from './home/home.component';
-import { InboxComponent } from './sidebar/inbox/inbox.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'auth', component: AuthenticationComponent},
   { path: 'dashboard', component: SidebarComponent },
-  { path: 'inbox', component: InboxComponent }
+  { path: '**', redirectTo:'/auth' }
 ];
 
 @NgModule({
